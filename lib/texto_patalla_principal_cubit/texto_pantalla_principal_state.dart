ccecb976623f2099510dcd717eea1b796414ad29
theRightHoopRod <unknown.rdm@gmail.com>
2024-10-23 22:10:49 -0600

    reestarted the proyect

--- lib/texto_patalla_principal_cubit/texto_pantalla_principal_state.dart
+++
@@ -1,8 +0,0 @@
-part of 'texto_pantalla_principal_cubit.dart';
-
-@immutable
-sealed class TextoPantallaPrincipalState {}
-
-final class TextoPantallaPrincipalInitial extends TextoPantallaPrincipalState {}
-
-final class TextoPantallPricipalMostrarRodrigo extends TextoPantallaPrincipalState {}
